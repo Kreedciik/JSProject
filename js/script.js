6838acf989873351715921fc01ce300629b18550
Ilfat <example@gmail.com>
2020-09-29 10:08:23 +0500

    Mod js file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// let obj = {
+
+//     name: 'John'
+// };
+
+// let descriptor = Object.getOwnPropertyDescriptor(obj, 'name');
+// console.log(descriptor);
+
+// Object.defineProperty(obj, 'name', {
+
+//     writable: false
+// });
+
+
+
